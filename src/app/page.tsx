fe2904e0c69d113fe253b445be634d7a41c14d03
Nadithi <moonasinghanadithi@gmail.com>
2026-04-26 17:40:02 +0530

    Added contact section to the pages.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import Hero from "@/components/Hero";
 import Projects from "@/components/Projects";
 import Skills from "@/components/Skills";
 import Leadership from "@/components/Leadership";
+import Contact from "@/components/Contact";
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       <Projects />
       <Skills />
       <Leadership />
+      <Contact />
     </main>
   );
 }
