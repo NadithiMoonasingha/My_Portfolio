cb6aa74214077ca5d6a75f588ed606d2e3ebbce1
Nadithi <moonasinghanadithi@gmail.com>
2026-06-20 01:30:01 +0530

    Fixed the frame size of the clinexis gallery

--- src/app/gallery/clinexis/page.tsx
+++ src/app/gallery/clinexis/page.tsx
@@ -13,6 +13,11 @@ export default function ClinexisGallery() {
         "/projects/Clinexis_ForgotPassword.jpeg",
         "/projects/Clinexis_CreateNewPassword.jpeg",
       ]}
+      frameWidth="100%"
+      frameMaxWidth="290px"
+      frameAspectRatio="9 / 20"
+      imageFit="contain"
+      gridColumns="repeat(auto-fit, minmax(220px, 1fr))"
     />
   );
 }
