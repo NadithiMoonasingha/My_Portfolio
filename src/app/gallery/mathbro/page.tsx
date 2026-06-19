87f1072f508c5db69a66287d227de16d70e7858c
Nadithi <moonasinghanadithi@gmail.com>
2026-06-20 02:03:49 +0530

    Fixed the frame size of the MathBro gallery

--- src/app/gallery/mathbro/page.tsx
+++ src/app/gallery/mathbro/page.tsx
@@ -9,6 +9,11 @@ export default function MathbroGallery() {
         "/projects/mathbro2.jpeg",
         "/projects/mathbro3.jpeg",
       ]}
-    />
-  );
+    frameWidth="100%"
+    frameMaxWidth="350px"
+    frameAspectRatio="9 / 11"
+    imageFit="contain"
+    gridColumns="repeat(auto-fit, minmax(220px, 1fr))"
+  />
+);
 }
